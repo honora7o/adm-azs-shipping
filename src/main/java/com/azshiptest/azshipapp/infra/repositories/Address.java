85a057f2ea98c72f5e59b0d2e9e39036a3b7e935
Mauricio Honorato <honorato_mauricio@hotmail.com>
2024-02-22 02:51:08 -0300

    Set up Postgres DB for testing purposes

--- src/main/java/com/azshiptest/azshipapp/infra/repositories/Address.java
+++ src/main/java/com/azshiptest/azshipapp/infra/repositories/Address.java
@@ -1,6 +1,14 @@
 package com.azshiptest.azshipapp.dto;
 
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public record Address(
+        @Id
+        Long id,
         String streetName,
         String neighbourhood,
         String city,
