9060b46108c5fa4de2a2e2396e29a3e63fbafcdd
Mauricio Honorato <honorato_mauricio@hotmail.com>
2024-02-22 00:30:15 -0300

    Added initial state for ShipmentInfoRepository

---
+++ src/main/java/com/azshiptest/azshipapp/infra/repositories/ShipmentInfoRepository.java
@@ -0,0 +1,6 @@
+package com.azshiptest.azshipapp.infra.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShipmentInfoRepository extends JpaRepository<ShipmentInfo, String> {
+}
