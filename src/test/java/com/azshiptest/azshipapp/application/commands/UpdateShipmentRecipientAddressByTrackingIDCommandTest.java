ae0944ee2a16c4d4f0825fcf972df32f741bc3c5
Mauricio Honorato <honorato_mauricio@hotmail.com>
2024-02-24 02:01:13 -0300

    Refactor UpdateShipmentRecipientAddressByTrackingIDCommandTest class to remove unecessary assertions

--- src/test/java/com/azshiptest/azshipapp/application/commands/UpdateShipmentRecipientAddressByTrackingIDCommandTest.java
+++ src/test/java/com/azshiptest/azshipapp/application/commands/UpdateShipmentRecipientAddressByTrackingIDCommandTest.java
@@ -5,34 +5,36 @@ import com.azshiptest.azshipapp.infra.repositories.adapters.ShipmentInfoReposito
 import com.azshiptest.azshipapp.models.Address;
 import com.azshiptest.azshipapp.models.ShipmentInfo;
 import com.azshiptest.azshipapp.models.StateCodeEnum;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@SpringBootTest
 public class UpdateShipmentRecipientAddressByTrackingIDCommandTest {
 
-    @Mock
     private ShipmentInfoRepository mockShipmentInfoRepository;
-    @Mock
     private AddressRepository mockAddressRepository;
-
     private UpdateShipmentRecipientAddressByTrackingIDCommand command;
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.initMocks(this);
+        mockShipmentInfoRepository = Mockito.mock(ShipmentInfoRepository.class);
+        mockAddressRepository = Mockito.mock(AddressRepository.class);
         command = new UpdateShipmentRecipientAddressByTrackingIDCommand(mockShipmentInfoRepository, mockAddressRepository);
     }
 
     @Test
-    void execute_ShouldUpdateRecipientAddressAndReturnOne_WhenShipmentInfoExists() {
+    void execute_ShouldUpdateRecipientAddress_WhenValidTrackingID() {
         String trackingID = "MG123456789SP";
         Address newRecipientAddress = Address.builder()
                 .streetName("Av Teste")
@@ -42,8 +44,8 @@ public class UpdateShipmentRecipientAddressByTrackingIDCommandTest {
                 .addressNumber(123)
                 .zipCode("12345")
                 .build();
+
         ShipmentInfo shipmentInfo = new ShipmentInfo();
-        shipmentInfo.setTrackingID(trackingID);
         Address oldRecipientAddress = Address.builder()
                 .streetName("Rua Teste")
                 .neighbourhood("Bairro Teste 2")
@@ -58,32 +60,29 @@ public class UpdateShipmentRecipientAddressByTrackingIDCommandTest {
 
         int result = command.execute(trackingID, newRecipientAddress);
 
+        verify(mockShipmentInfoRepository, times(1)).findByTrackingID(trackingID);
+        verify(mockAddressRepository, times(1)).updateAddressById(eq(shipmentInfo.getRecipientAddress().getId()), eq("Av Teste"), eq("Bairro Teste 1"), eq("Sao Paulo"), eq(StateCodeEnum.SP), eq(123), eq("12345"));
         assertEquals(1, result);
+    }
 
-        ArgumentCaptor<Long> addressIdCaptor = ArgumentCaptor.forClass(Long.class);
-        ArgumentCaptor<String> streetNameCaptor = ArgumentCaptor.forClass(String.class);
-        ArgumentCaptor<String> neighbourhoodCaptor = ArgumentCaptor.forClass(String.class);
-        ArgumentCaptor<String> cityCaptor = ArgumentCaptor.forClass(String.class);
-        ArgumentCaptor<StateCodeEnum> stateCodeEnumCaptor = ArgumentCaptor.forClass(StateCodeEnum.class);
-        ArgumentCaptor<Integer> addressNumberCaptor = ArgumentCaptor.forClass(Integer.class);
-        ArgumentCaptor<String> zipCodeCaptor = ArgumentCaptor.forClass(String.class);
+    @Test
+    void execute_ShouldReturnZero_WhenInvalidTrackingID() {
+        String trackingID = "INVALIDTRACKINGID";
+        Address newRecipientAddress = Address.builder()
+                .streetName("Av Teste")
+                .neighbourhood("Bairro Teste 1")
+                .city("Sao Paulo")
+                .stateCodeEnum(StateCodeEnum.SP)
+                .addressNumber(123)
+                .zipCode("12345")
+                .build();
 
-        verify(mockAddressRepository).updateAddressById(
-                addressIdCaptor.capture(),
-                streetNameCaptor.capture(),
-                neighbourhoodCaptor.capture(),
-                cityCaptor.capture(),
-                stateCodeEnumCaptor.capture(),
-                addressNumberCaptor.capture(),
-                zipCodeCaptor.capture()
-        );
+        when(mockShipmentInfoRepository.findByTrackingID(trackingID)).thenReturn(Optional.empty());
 
-        assertEquals(oldRecipientAddress.getId(), addressIdCaptor.getValue());
-        assertEquals(newRecipientAddress.getStreetName(), streetNameCaptor.getValue());
-        assertEquals(newRecipientAddress.getNeighbourhood(), neighbourhoodCaptor.getValue());
-        assertEquals(newRecipientAddress.getCity(), cityCaptor.getValue());
-        assertEquals(newRecipientAddress.getStateCodeEnum(), stateCodeEnumCaptor.getValue());
-        assertEquals(newRecipientAddress.getAddressNumber(), addressNumberCaptor.getValue());
-        assertEquals(newRecipientAddress.getZipCode(), zipCodeCaptor.getValue());
+        int result = command.execute(trackingID, newRecipientAddress);
+
+        verify(mockShipmentInfoRepository, times(1)).findByTrackingID(trackingID);
+        verify(mockAddressRepository, times(0)).updateAddressById(anyLong(), anyString(), anyString(), anyString(), any(), anyInt(), anyString());
+        assertEquals(0, result);
     }
 }
