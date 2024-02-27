49a3df962696023399778fecb89cbfdf94e7d97f
Mauricio Honorato <honorato_mauricio@hotmail.com>
2024-02-27 01:13:31 -0300

    Remove shipmententity from pageable response object

--- src/main/java/com/azshiptest/azshipapp/application/queries/FindAllShipmentsByTaxPayerRegistrationNoQuery.java
+++ src/main/java/com/azshiptest/azshipapp/application/queries/FindAllShipmentsByTaxPayerRegistrationNoQuery.java
@@ -1,11 +1,14 @@
 package com.azshiptest.azshipapp.application.queries;
 
+import com.azshiptest.azshipapp.dto.ShipmentDTO;
 import com.azshiptest.azshipapp.dto.ShipmentInfoPageableResponse;
 import com.azshiptest.azshipapp.infra.repositories.adapters.ShipmentRepository;
 import com.azshiptest.azshipapp.infra.entities.ShipmentEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class FindAllShipmentsByTaxPayerRegistrationNoQuery {
@@ -17,8 +20,11 @@ public class FindAllShipmentsByTaxPayerRegistrationNoQuery {
 
     public ShipmentInfoPageableResponse execute(String taxPayerRegistrationNo, Pageable pageable) {
         Page<ShipmentEntity> shipmentInfo = shipmentRepository.findAllByTaxPayerRegistrationNo(taxPayerRegistrationNo, pageable);
+
+        List<ShipmentDTO> shipmentDTOs = ShipmentDTO.mapEntitiesToDTOs(shipmentInfo.getContent());
+
         ShipmentInfoPageableResponse response = new ShipmentInfoPageableResponse(
-                shipmentInfo.getContent(),
+                shipmentDTOs,
                 shipmentInfo.getPageable().getPageNumber(),
                 shipmentInfo.getPageable().getPageSize(),
                 shipmentInfo.getTotalElements(),
--- src/main/java/com/azshiptest/azshipapp/dto/ShipmentDTO.java
+++ src/main/java/com/azshiptest/azshipapp/dto/ShipmentDTO.java
@@ -1,10 +1,13 @@
 package com.azshiptest.azshipapp.dto;
 
+import com.azshiptest.azshipapp.infra.entities.ShipmentEntity;
 import com.azshiptest.azshipapp.models.ShipmentStatusEnum;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public record ShipmentDTO(UUID trackingNo,
                           String taxPayerRegistrationNo,
@@ -17,4 +20,24 @@ public record ShipmentDTO(UUID trackingNo,
                           Integer weight,
                           Float cubingMeasurement
 ) {
+    public static ShipmentDTO fromEntity(ShipmentEntity entity) {
+        return new ShipmentDTO(
+                entity.getTrackingNo(),
+                entity.getTaxPayerRegistrationNo(),
+                AddressDTO.fromEntity(entity.getSenderAddress()),
+                AddressDTO.fromEntity(entity.getRecipientAddress()),
+                entity.getShipmentStatus(),
+                entity.getPostingDate(),
+                entity.getEstimatedArrivalDate(),
+                entity.getValue(),
+                entity.getWeight(),
+                entity.getCubingMeasurement()
+        );
+    }
+
+    public static List<ShipmentDTO> mapEntitiesToDTOs(List<ShipmentEntity> entities) {
+        return entities.stream()
+                .map(ShipmentDTO::fromEntity)
+                .collect(Collectors.toList());
+    }
 }
